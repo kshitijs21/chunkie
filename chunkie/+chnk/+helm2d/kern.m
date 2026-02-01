d60ef52a4c1bed9c059a503b944da0baa6b1f64f
Kshitij Sinha <sinhakshitij377@gmail.com>
2026-02-02 00:19:07 +0530

    Added Frequency derivative Single Layer in the helm2d.kern

--- chunkie/+chnk/+helm2d/kern.m
+++ chunkie/+chnk/+helm2d/kern.m
@@ -111,6 +111,32 @@ if strcmpi(type,'freq_diff')
     submat = zk*val.*( (rx.*nx) + (ry.*ny) ); % grad(:,:,2).*ny);
 end
 
+
+% single layer (frequency difference)
+if strcmpi(type,'fd_s')
+    [~,ns] = size(src);
+    [~,nt] = size(targ);
+
+    xs = repmat(src(1,:),nt,1);
+    ys = repmat(src(2,:),nt,1);
+
+    xt = repmat(targ(1,:).',1,ns);
+    yt = repmat(targ(2,:).',1,ns);
+
+    rx = xt-xs;
+    ry = yt-ys;
+
+    rx2 = rx.*rx;
+    ry2 = ry.*ry;
+
+    r2 = rx2+ry2;
+
+    r = sqrt(r2);
+
+    submat = -0.25*1i*besselh(1,1,zk*r).*r;
+end
+
+
 % normal derivative of single layer
 if strcmpi(type,'sprime')
   targnorm = targinfo.n(:,:);
